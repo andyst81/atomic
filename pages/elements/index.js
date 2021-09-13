92754e58cbc45c7ef69545267362ed8a05fbc6fe
andy <andyst81@users.noreply.github.com>
2021-09-13 15:53:34 +0700

    fix small bugs

--- pages/elements/index.js
+++ pages/elements/index.js
@@ -1,29 +1,29 @@
-import fetch from 'node-fetch'
-// import UserList from '../components/UserList'
+// import fetch from 'node-fetch'
+// // import UserList from '../components/UserList'
 
-const HomePage = ({ tokenData }) => {
-  return (
-    <div className="home-page">
-      <TokenList tokenData={tokenData} />
-    </div>
-  )
-}
+// const HomePage = ({ tokenData }) => {
+//   return (
+//     <div className="home-page">
+//       <TokenList tokenData={tokenData} />
+//     </div>
+//   )
+// }
 
-export const getServerSideProps = async ({ query }) => {
-  // Fetch the first page as default
-  const page = query.page || 1
-  let tokenData = null
-  // Fetch data from external API
-  try {
-    const res = await fetch(`${process.env.FETCH_URL}/api/elements/Aluminium?page=${page}`)
-    if (res.status !== 200) {
-      throw new Error('Failed to fetch')
-    }
-    userData = await res.json()
-  } catch (err) {
-    userData = { error: { message: err.message } }
-  }
-  // Pass data to the page via props
-  return { props: { tokenData } }
-}
-export default HomePage
+// export const getServerSideProps = async ({ query }) => {
+//   // Fetch the first page as default
+//   const page = query.page || 1
+//   let tokenData = null
+//   // Fetch data from external API
+//   try {
+//     const res = await fetch(`${process.env.FETCH_URL}/api/elements/Aluminium?page=${page}`)
+//     if (res.status !== 200) {
+//       throw new Error('Failed to fetch')
+//     }
+//     userData = await res.json()
+//   } catch (err) {
+//     userData = { error: { message: err.message } }
+//   }
+//   // Pass data to the page via props
+//   return { props: { tokenData } }
+// }
+// export default HomePage
